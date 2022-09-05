8d149fb52b63900df85cf6da0801aec7f18955d5
kimjiho1125 <kimjiho1125a@naver.com>
2022-09-05 23:49:40 +0900

    따배씨

--- 따배씨/studyc++/Chapter6_14/Chapter6_14.cpp
+++ 따배씨/studyc++/Chapter6_14/Chapter6_14.cpp
@@ -2,8 +2,39 @@
 
 using namespace std;
 
+struct Something
+{
+	int v1;
+	float v2;
+};
+
+struct Other
+{
+	Something st;
+};
+
+//void doSomething(int &n)
+//{
+//	n = 10;
+//	cout << "In doSomething " << n << endl;
+//}
+
 int main()
 {
+	/*int n = 5;
+	cout << n << endl;
+
+	doSomething(n);
+	cout << n << endl;*/
+
+	Other ot;
+
+	int& v1 = ot.st.v1;
+	v1 = 1;
+
+	int value = 5;
+	int* const ptr = &value;
+	int& ref = value;
 
 	return 0;
 }
