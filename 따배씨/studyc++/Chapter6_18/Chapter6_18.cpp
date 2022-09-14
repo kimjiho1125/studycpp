da52433365c15f29bf030fae086ca4b57aae8460
kimjiho1125 <kimjiho1125a@naver.com>
2022-09-14 23:11:52 +0900

    따배씨

--- 따배씨/studyc++/Chapter6_18/Chapter6_18.cpp
+++ 따배씨/studyc++/Chapter6_18/Chapter6_18.cpp
@@ -4,6 +4,18 @@ using namespace std;
 
 int main()
 {
+	int i = 5;
+	float f = 3.0;
+	char c = 'a';
+
+	void *ptr = nullptr;
+
+	ptr = &i;
+	ptr = &f;
+	//ptr = &c;
+
+	cout << &f << " " << ptr << endl;
+	cout << *static_cast<float*>(ptr) << endl;
 
 	return 0;
 }
