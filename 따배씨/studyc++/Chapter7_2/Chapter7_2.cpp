c7c2c356db195bedefcd1037d752a2d33724e182
kimjiho1125 <kimjiho1125a@naver.com>
2022-10-06 23:35:48 +0900

    따배씨

--- 따배씨/studyc++/Chapter7_2/Chapter7_2.cpp
+++ 따배씨/studyc++/Chapter7_2/Chapter7_2.cpp
@@ -2,8 +2,21 @@
 
 using namespace std;
 
+void doSomething(int y) 
+{
+	cout << "In func " << y << " " << &y << endl;
+}
+
 int main()
 {
+	doSomething(5);
+
+	int x = 6;
+
+	cout << "In main " << x << " " << &x << endl;
+
+	doSomething(x);
+	doSomething(x + 1);
 
 	return 0;
 }
